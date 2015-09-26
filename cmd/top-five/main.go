01d60bc7a6f1c12c3049acb138a4ba02ab2abd9d
Hans Sjunnesson <hans.sjunnesson@gmail.com>
2015-09-26 21:02:17 +0200

    Fix

--- cmd/top-five/main.go
+++ cmd/top-five/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/hsjunnesson/top-five/Godeps/_workspace/src/github.com/gin-gonic/gin"
 )
 
 func main() {
-	var err error
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -17,12 +15,12 @@ func main() {
 	}
 	
 	router := gin.New()
-	route.Use(gin.Logger())
+	router.Use(gin.Logger())
 	
-	r.GET("/ping", func(c *gin.Context) {
+	router.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
 	
-	r.Run(":" + port)
+	router.Run(":" + port)
 }
 

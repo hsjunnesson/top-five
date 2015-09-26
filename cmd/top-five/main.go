bf89a71827c4ba1c0194a48e6362da2f795e8965
Hans Sjunnesson <hans.sjunnesson@gmail.com>
2015-09-26 20:53:17 +0200

    Update

--- cmd/top-five/main.go
+++ cmd/top-five/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/hsjunnesson/top-five/Godeps/_workspace/src/github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
+	var err error
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	
+	router := gin.New()
+	route.Use(gin.Logger())
+	
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
-	r.Run(":8080")
+	
+	r.Run(":" + port)
 }
+

ea166e416870b7286bae84a409c578d9c37b190e
Hans Sjunnesson <hans.sjunnesson@screeninteraction.com>
2015-10-01 15:56:32 +0200

    Static file.

--- main.go
+++ main.go
@@ -14,11 +14,11 @@ func main() {
 	}
 
 	app := gin.Default()
+
+	app.Static("/", "public")
 	app.GET("/lists", ListsList)
 	app.GET("/lists/:list_id", ListsDetail)
 
-	app.Static("/public", "./public")
-	app.Static("/1726D19535E7EF78AD05045709BA5EA1.txt", "./public/1726D19535E7EF78AD05045709BA5EA1.txt")
 	
 	app.Run(":" + port)
 

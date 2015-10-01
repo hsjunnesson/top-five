559aaead6a566ad087015baab8211ad00368ee0c
Hans Sjunnesson <hans.sjunnesson@screeninteraction.com>
2015-10-01 10:01:39 +0200

    Fix

--- lists_test.go
+++ lists_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestLists(t *testing.T) {
 	list := List{Id: "", Title: "This is a title", Items: [5]string{"one", "two", "three", "four", "five"}, Timestamp: time.Now()}
-	id, err := CreateList(redisClient, list)
+	_, err := CreateList(redisClient, list)
 	if err != nil {
 		t.Fatal(err)
 	}

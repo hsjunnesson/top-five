815bd85fe4fbc1d75dbcee51c9830409ab6e0bd5
Hans Sjunnesson <hans.sjunnesson@gmail.com>
2015-10-03 01:06:15 +0200

    Finished basic CRUD for lists.

--- list.go
+++ list.go
@@ -4,9 +4,13 @@ import (
 	"time"
 	"log"
 	"strconv"
+	"errors"
 	"github.com/hsjunnesson/top-five/Godeps/_workspace/src/github.com/gin-gonic/gin"
 	"github.com/hsjunnesson/top-five/Godeps/_workspace/src/gopkg.in/redis.v3"
 )
+
+
+// Types
 
 type List struct {
 	Id          string
@@ -16,6 +20,9 @@ type List struct {
 }
 
 type Lists []List
+
+
+// Database functions
 
 // Creates a List in the connected redis.
 // Returns the id of the List, or error.
@@ -41,13 +48,13 @@ func CreateListDB(client *redis.Client, title string, listItems [5]string) (stri
 	}
 	
 	for _, val := range listItems {
-		if client.RPush("list_items:" + strId, val).Err() != nil {
+		if err := client.RPush("list_items:" + strId, val).Err(); err != nil {
 			log.Printf("Couldn't RPush list_items:" + strId, err)
 			return "", err
 		}
 	}
 	
-	if client.LPush("lists", strId).Err() != nil {
+	if err := client.LPush("lists", strId).Err(); err != nil {
 		log.Printf("Couldn't LPush lists " + strId, err)
 		return "", err
 	}
@@ -55,36 +62,90 @@ func CreateListDB(client *redis.Client, title string, listItems [5]string) (stri
 	return strId, nil
 }
 
+func IsListDeletedDB(client *redis.Client, id string) (bool, error) {
+	isMember, err := client.SIsMember("deleted_lists", id).Result()
+	if err != nil {
+		log.Printf("Couldn't SISMember deleted_lists " + id)
+		return false, err
+	}
+
+	return isMember, nil
+}
+
 // Fetches a List from the connected redis.
 func GetListDB(client *redis.Client, id string) (*List, error) {
+	// First check if list has been marked as deleted
+	isDeleted, err := IsListDeletedDB(client, id)
+	if err != nil {
+		return nil, err
+	}
+
+	if isDeleted {
+		return nil, nil
+	}
+
+	// Fetch parts from redis
 	parts, err := client.HMGet("list:" + id, "id", "title", "created_at").Result()
 	if err != nil {
 		log.Printf("Could not HMGet list:" + id, err)
 		return nil, err
 	}
 
-	id, err := parts[0].(string)
+	listId, ok := parts[0].(string)
+	if !ok {
+		return nil, errors.New("Not ok")
+	}
+
+	title, ok := parts[1].(string)
+	if !ok {
+		return nil, errors.New("Not ok")
+	}
+
+	created_at, ok := parts[2].(string)
+	if !ok {
+		return nil, errors.New("Not ok")
+	}
+
+	created_at_time, err := time.Parse(time.RFC3339, created_at)
 	if err != nil {
+		log.Printf("Couldn't parse time: " + created_at, err)
 		return nil, err
 	}
 
-	title, err := parts[1].(string)
+	items, err := client.LRange("list_items:" + id, 0, 5).Result()
 	if err != nil {
+		log.Printf("Couldn't LRange list_items:" + id, err)
 		return nil, err
 	}
 
-		
-	
-	var items = [5]string{"one", "two", "three", "four", "five"}
+	if len(items) != 5 {
+		log.Printf("List items not five elements")
+		return nil, errors.New("List items not five elements")
+	}
+
 	list := List{
-		Id: parts[0],
-		Title: parts[1],
-		CreatedAt: time.New(parts[2]),
-		ListItems: items,
+		Id: listId,
+		Title: title,
+		CreatedAt: created_at_time,
+		ListItems: [5]string{items[0], items[1], items[2], items[3], items[4]},
 	}
 
-	return list, nil
+	return &list, nil
 }
+
+// Deletes a list from the connected redis.
+// This doesn't actually delete it, just marks it as deleted.
+func DeleteListDB(client *redis.Client, id string) error {
+	if err := client.SAdd("deleted_lists", id).Err(); err != nil {
+		log.Printf("Couldn't SAdd deleted_lists " + id, err)
+		return err
+	}
+
+	return nil
+}
+
+
+// Route handlers
 
 func ListsList(c *gin.Context) {
 	c.JSON(200, "Hej")
